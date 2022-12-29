f9386c1f88b4a9410d739d5c6b4210b302d71f8e
Erione White <erionewhite1999@gmail.com>
2022-12-28 20:47:40 -0600

    menu button opens/closes menu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let menuBtn = document.querySelector('.js-menu-btn')
+let menu = document.querySelector('.js-menu')
 
 menuBtn.addEventListener('click', () => {
-    
+    menu.classList.toggle('visible')
 })
