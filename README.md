796d2de55686c11cfb6d75180e12ff6837664183
Erione White <erionewhite1999@gmail.com>
2023-01-02 19:47:41 -0600

    Added markup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hamburger Menu Project
+This project demonstrates a responsive hamburger menu. 
+
+## How It Works
+When the hamburger icon is clicked, a menu with links will appear on the screen. When each link is clicked, the contents of the page will change to an image of the item clicked. 
+
+To exit out of the menu, the user can click the menu icon again. The menu will slide off-screen, and will no longer be visible. 
--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 let menuBtn = document.querySelector('.js-menu-btn')
 let menu = document.querySelector('.js-menu')
+let links = document.querySelectorAll('.js-menu-link')
+let img = document.querySelector('.js-img')
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('visible')
+})
+
+function handleClickEvents(e){
+    let flowerName = e.target.innerText.toLowerCase()
+    flowerName = flowerName.replace(/\s+/g, '')
+    img.style.backgroundImage = `url(./photos/${flowerName}.jpg)`
+    menu.classList.remove('visible')
+}
+
+links.forEach(link => {
+    link.addEventListener('click', handleClickEvents)
 })
